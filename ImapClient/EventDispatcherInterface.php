0597cd45a0be49f679f8338583c3da8dbc16fd2a
sergey144010 <sergey144010@users.noreply.github.com>
2018-08-25 11:11:40 +0300

    Update EventDispatcherInterface.php

--- ImapClient/EventDispatcherInterface.php
+++ ImapClient/EventDispatcherInterface.php
@@ -4,7 +4,7 @@ namespace sergey144010\ImapClient;
 
 interface EventDispatcherInterface
 {
-    # https://framework.zend.com/manual/2.4/en/modules/zend.event-manager.event-manager.html
+    # https://docs.zendframework.com/zend-eventmanager/quick-start/
     public function attach(string $object, string $event, callable $handler);
     public function trigger(string $name, object $object, $params);
 }
