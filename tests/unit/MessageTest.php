a9396641805b20c899d3b0236ae0a226784e765c
employee <employee>
2018-10-18 17:57:12 +0300

    Create unit test

---
+++ tests/unit/MessageTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: employee
+ * Date: 18.10.2018
+ * Time: 17:50
+ */
+
+namespace sergey144010\ImapClient\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+use sergey144010\ImapClient\Incoming\Message;
+
+#C:\projects\php-imap-client>vendor\bin\phpunit tests\MessageTest.php
+class MessageTest extends TestCase
+{
+    public function testCreateMessage()
+    {
+        $message = new Message();
+        $message->setHeaders('123');
+        $this->assertEquals('123', $message->getHeaders());
+    }
+}
