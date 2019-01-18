3843b44b7fce463131d512a5c5fba842978066cc
employee <employee>
2019-01-18 17:12:15 +0300

    Progress

---
+++ ImapClient/Incoming/Interfaces/MessageStructureInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: employee
+ * Date: 18.01.2019
+ * Time: 16:56
+ */
+
+namespace sergey144010\ImapClient\Incoming\Interfaces;
+
+
+class MessageStructureInterface
+{
+
+}
---
+++ ImapClient/Incoming/MessageStructure.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: employee
+ * Date: 18.01.2019
+ * Time: 17:02
+ */
+
+namespace sergey144010\ImapClient\Incoming;
+
+
+class MessageStructure
+{
+
+}
